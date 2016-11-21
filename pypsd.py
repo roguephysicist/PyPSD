a413bb01aa01082a76ab48dcc22a2db8a0b925d8
Sean Anderson <sean.martin.anderson@gmail.com>
2016-11-21 23:36:50 +0100

    lightning fast and complete

--- pypsd.py
+++ pypsd.py
@@ -1,15 +1,12 @@
 #!/Users/sma/anaconda3/bin/python
 import numpy as np
+# import matplotlib.pyplot as plt
+# from matplotlib.ticker import ScalarFormatter
 import sys
 
 infile = sys.argv[1]
 particles = np.loadtxt(infile)
 dmin, dmax, bins = np.loadtxt("bins.dat", unpack=True)
-
-def cumulative(freq):
-    pct = freq * (1/np.sum(freq)) * 100
-    cum = np.cumsum(pct, dtype=float)
-    return cum
 
 def distribution(values, cutoff):
     n = np.argmax(values>cutoff)
@@ -29,16 +26,88 @@ ni = np.bincount(np.digitize(diameters, bins), minlength=bins.size) # dmin
 ai = 4 * np.pi * (bins/2)**2 * ni
 vi = (4.0/3.0) * np.pi * (bins/2)**3 * ni
 
-ncum = cumulative(ni)
-acum = cumulative(ai)
-vcum = cumulative(vi)
+# Differential percentage
+npct = ni * (1/np.sum(ni)) * 100
+apct = ai * (1/np.sum(ai)) * 100
+vpct = vi * (1/np.sum(vi)) * 100
 
-print(6*" " + 'Number' + 10*" " + 'Surface' + 9*" " + 'Volume')
-print(54*"-")
-print('D10: {0:15.12f} {1:15.12f} {2:15.12f}'.format(distribution(ncum, 10), distribution(acum, 10), distribution(vcum, 10)))
-print('D50: {0:15.12f} {1:15.12f} {2:15.12f}'.format(distribution(ncum, 50), distribution(acum, 50), distribution(vcum, 50)))
-print('D90: {0:15.12f} {1:15.12f} {2:15.12f}'.format(distribution(ncum, 90), distribution(acum, 90), distribution(vcum, 90)))
+# Cumulative percentage
+ncum = np.cumsum(npct, dtype=float)
+acum = np.cumsum(apct, dtype=float)
+vcum = np.cumsum(vpct, dtype=float)
 
+# Number distributions
+nD10 = distribution(ncum, 10)
+nD50 = distribution(ncum, 50)
+nD90 = distribution(ncum, 90)
 
-# data = np.column_stack([bins, vcum])
-# np.savetxt("out.dat", data)
+# Area distributions
+aD10 = distribution(acum, 10)
+aD50 = distribution(acum, 50)
+aD90 = distribution(acum, 90)
+
+# Volume distributions
+vD10 = distribution(vcum, 10)
+vD50 = distribution(vcum, 50)
+vD90 = distribution(vcum, 90)
+
+# Span
+nspan = (nD90 - nD10)/nD50 
+aspan = (aD90 - aD10)/aD50 
+vspan = (vD90 - vD10)/vD50 
+
+# Mode
+nmode = bins[np.argmax(npct)]
+amode = bins[np.argmax(apct)]
+vmode = bins[np.argmax(vpct)]
+
+# Median
+nmedian = bins[np.argmax(ncum>=50)]
+amedian = bins[np.argmax(acum>=50)]
+vmedian = bins[np.argmax(vcum>=50)]
+
+# D[1,0], D[3,2], D[4,3]
+D_1_0 = np.sum(ni * bins)/np.sum(ni)
+D_3_2 = np.sum(ni * bins**3)/np.sum(ni * bins**2)
+D_4_3 = np.sum(ni * bins**4)/np.sum(ni * bins**3)
+# D_1_0 = np.sum(diameters)/np.sum(ni)
+# D_3_2 = np.sum(diameters**3)/np.sum(diameters**2)
+# D_4_3 = np.sum(diameters**4)/np.sum(diameters**3)
+
+print('\nTotal number of particles: {0}\n'.format(particles.size))
+print(9*" " + 'Number' + 4*" " + 'Surface' + 3*" " + 'Volume')
+print(36*"=")
+print('D10:    {0:6.3f}    {1:6.3f}    {2:6.3f}'.format(nD10, aD10, vD10))
+print('D50:    {0:6.3f}    {1:6.3f}    {2:6.3f}'.format(nD50, aD50, vD50))
+print('D90:    {0:6.3f}    {1:6.3f}    {2:6.3f}'.format(nD90, aD90, vD90))
+print(36*"-")
+print('Span:   {0:6.3f}    {1:6.3f}    {2:6.3f}'.format(nspan, aspan, vspan))
+print('Mode:   {0:6.3f}    {1:6.3f}    {2:6.3f}'.format(nmode, amode, vmode))
+print('Median: {0:6.3f}    {1:6.3f}    {2:6.3f}'.format(nmedian, amedian, vmedian))
+print(36*"-")
+print('D[1,0]: {0:6.3f}'.format(D_1_0))
+print('D[3,2]: {0:6.3f}'.format(D_3_2))
+print('D[4,3]: {0:6.3f}'.format(D_4_3))
+print()
+
+# plt.figure(1)
+
+# plt.subplot(211)
+# plt.grid(True)
+# plt.title("Sample Average")
+# plt.xlabel("Samples")
+# plt.ylabel("Cumulative Average")
+# plt.plot(avgcum, lw=2.0)
+
+# plt.subplot(212)
+# # ax.xaxis.set_major_formatter(ScalarFormatter())
+# plt.title("Droplet Size Distribution")
+# plt.xlabel("Diameter (um)")
+# plt.ylabel("Differential (%)")
+# plt.grid(True)
+# # plt.xlim((0,10))
+# plt.xscale('log')
+# plt.plot(bins, npct, 'red', bins, apct, 'purple', bins, vpct, 'green', lw=2.5)
+
+# # plt.tight_layout()
+# plt.show()
